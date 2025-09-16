473554fd8ba246bee41beea5b437a9a4e6261db6
juvascoli <jvascoli6@gmail.com>
2025-09-16 10:50:39 -0300

    initializing pages

---
+++ CheckPoint4/src/services/firebaseConfig.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0zgZIl0rynCroftBBWTS19ExbSlsS7KM",
+  authDomain: "projetofirebase-844c4.firebaseapp.com",
+  projectId: "projetofirebase-844c4",
+  storageBucket: "projetofirebase-844c4.firebasestorage.app",
+  messagingSenderId: "79687662351",
+  appId: "1:79687662351:web:efea872ef6e8bea16f5c7a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
